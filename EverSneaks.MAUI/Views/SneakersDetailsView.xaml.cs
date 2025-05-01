23ea757720986c8517d124d4791ee9a9f1306fd4
Matthew Robbins <matthew.ch.robbins@gmail.com>
2025-05-01 12:18:07 +1000

    Example runtime model loading.

--- EverSneaks.MAUI/Views/SneakersDetailsView.xaml.cs
+++ EverSneaks.MAUI/Views/SneakersDetailsView.xaml.cs
@@ -41,4 +41,118 @@ public partial class SneakersDetailsView : ContentPage
     {
         base.OnDisappearing();
     }
+
+    private bool isLoading = false;
+
+    private class ModelToLoad
+    {
+        public string Description { get; set; }
+        public string FileName { get; set; }
+        public string Url { get; set; } 
+    }
+    
+
+    private async void Button_OnClicked(object sender, EventArgs e)
+    {
+        if (isLoading)
+        {
+            return;
+        }
+
+        try
+        {
+
+            List<ModelToLoad> models = new List<ModelToLoad>()
+            {
+                new ModelToLoad()
+                {
+                    Description = "Elphinestone (Polycam - Default scene)",
+                    FileName = "elphinestone.glb",
+                    Url = "https://redpointdemo.blob.core.windows.net/data/elphinestone_fixed.glb"
+                },
+                new ModelToLoad()
+                {
+                    Description = "Elphinestone Optmised (Polycam, Optimised w gltf-transform optimize)",
+                    FileName = "elphinestone_optimised.glb",
+                    Url = "https://redpointdemo.blob.core.windows.net/data/elphinestone_optimised.glb"
+                },
+                new ModelToLoad()
+                {
+                    Description = "Gibraltar (Polycam - No default scene)",
+                    FileName = "gibraltar.glb",
+                    Url = "https://redpointdemo.blob.core.windows.net/data/gibraltar.glb"
+                },
+                new ModelToLoad()
+                {
+                    Description = "Helmet (GLB Official Sample)",
+                    FileName = "helmet.glb",
+                    Url = "https://redpointdemo.blob.core.windows.net/data/DamagedHelmet.glb"
+                },
+                new ModelToLoad()
+                {
+                    Description = "Toy Car (GLB Official Sample)",
+                    FileName = "ToyCar.glb",
+                    Url = "https://redpointdemo.blob.core.windows.net/data/ToyCar.glb"
+                },
+                new ModelToLoad()
+                {
+                    Description = "Air Jordan (Evergine EverSneak Sample)",
+                    FileName = "airjordan.glb",
+                    Url = "https://redpointdemo.blob.core.windows.net/data/AirJordan.glb"
+                },
+            };
+            
+            await DisplayAlert("Please select a model to dynamically load from blob storage.", "Select a model to load", "OK");
+            
+            var choice = await DisplayActionSheet("Select a model to load", "Cancel", null, models.Select(item => item.Description).ToArray());
+            
+            var model = models.FirstOrDefault(item => item.Description == choice);
+            if (model == null)
+            {
+                return;
+            }
+
+            isLoading = true;
+            
+            const string key = "airjordan.glb";
+            
+            Console.WriteLine($"Loading {model.Description}");
+
+            var fileName = model.FileName;
+            var url = model.Url;
+            
+            string filePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+            if (File.Exists(filePath))
+            {
+                
+                var shouldDelete = await DisplayAlert("Model Already Downloaded", "Do you want to delete the existing local file and redownload it?", "Delete and re-download", "Continue with existing file");
+                if (shouldDelete)
+                {
+                    File.Delete(filePath);
+                }
+            }
+
+            if (!File.Exists(filePath))
+            {
+
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(url);
+                    var content = await response.Content.ReadAsByteArrayAsync();
+                    await File.WriteAllBytesAsync(filePath, content);
+                }
+            }
+
+            await evergineApplication.LoadGlb(filePath);
+
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
 }
--- EverSneaks/MyApplication.cs
+++ EverSneaks/MyApplication.cs
@@ -1,12 +1,24 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Evergine.Common.Graphics;
 using Evergine.Common.IO;
 using Evergine.Framework;
+using Evergine.Framework.Graphics;
+using Evergine.Framework.Graphics.Effects;
+using Evergine.Framework.Graphics.Materials;
+using Evergine.Framework.Runtimes;
 using Evergine.Framework.Services;
 using EverSneaks.Services;
+using Random = Evergine.Framework.Services.Random;
 
 namespace EverSneaks
 {
     public partial class MyApplication : Application
     {
+        MyScene scene;
+        
         public MyApplication()
         {
             this.Container.Register<Settings>();
@@ -32,10 +44,107 @@ namespace EverSneaks
             var assetsService = this.Container.Resolve<AssetsService>();
 
             // Navigate to scene
-            var scene = assetsService.Load<MyScene>(EvergineContent.Scenes.MyScene_wescene);
+            scene = assetsService.Load<MyScene>(EvergineContent.Scenes.MyScene_wescene);
             ScreenContext screenContext = new ScreenContext(scene);
             screenContextManager.To(screenContext);
         }
+
+        public async Task LoadGlb(string filePath)
+        {
+            var assetsService = Application.Current.Container.Resolve<AssetsService>();
+            var assetsDirectory = Application.Current.Container.Resolve<AssetsDirectory>();
+            
+            Model model = null;
+            using (var fileStream = File.OpenRead(filePath))
+            {
+                model = await Evergine.Runtimes.GLB.GLBRuntime.Instance.Read(fileStream, CustomMaterialAssigner);
+            }
+            
+            var entity = model.InstantiateModelHierarchy(assetsService);
+            var screenContextManager = this.Container.Resolve<ScreenContextManager>();
+
+            entity.Name = "Runtime loaded GLB";
+            
+            scene.Managers.EntityManager.Add(entity);
+        }
+        
+        private async Task<Material> CustomMaterialAssigner(MaterialData data)
+{
+    var assetsService = Application.Current.Container.Resolve<AssetsService>();
+
+    // Get textures            
+    var baseColor = await data.GetBaseColorTextureAndSampler();
+    var metallicRoughness = await data.GetMetallicRoughnessTextureAndSampler();
+    var normalTex = await data.GetNormalTextureAndSampler();  
+    var emissive = await data.GetEmissiveTextureAndSampler();
+    var occlussion = await data.GetOcclusionTextureAndSampler();            
+
+    // Get Layer
+    var opaqueLayer = assetsService.Load<RenderLayerDescription>(DefaultResourcesIDs.OpaqueRenderLayerID);
+    var alphaLayer = assetsService.Load<RenderLayerDescription>(DefaultResourcesIDs.AlphaRenderLayerID);
+    RenderLayerDescription layer;
+    float alpha = data.BaseColor.A / 255.0f;
+    switch (data.AlphaMode)
+    {
+        default:
+        case Evergine.Framework.Runtimes.AlphaMode.Mask:
+        case Evergine.Framework.Runtimes.AlphaMode.Opaque:
+            layer = opaqueLayer;
+            break;
+        case Evergine.Framework.Runtimes.AlphaMode.Blend:
+            layer = alphaLayer;
+            break;
+    }
+
+    // Create standard material            
+    var effect = assetsService.Load<Effect>(DefaultResourcesIDs.StandardEffectID);            
+    StandardMaterial standard = new StandardMaterial(effect)
+    {
+        LightingEnabled = data.HasVertexNormal,
+        IBLEnabled = data.HasVertexNormal,
+        BaseColor = data.BaseColor,
+        Alpha = alpha,
+        BaseColorTexture = baseColor.Texture,
+        BaseColorSampler = baseColor.Sampler,
+        Metallic = data.MetallicFactor,
+        Roughness = data.RoughnessFactor,
+        MetallicRoughnessTexture = metallicRoughness.Texture,
+        MetallicRoughnessSampler = metallicRoughness.Sampler,
+        EmissiveColor = data.EmissiveColor.ToColor(),
+        EmissiveTexture = emissive.Texture,
+        EmissiveSampler = emissive.Sampler,
+        OcclusionTexture = occlussion.Texture,
+        OcclusionSampler = occlussion.Sampler,
+        LayerDescription = layer,                
+    };
+
+    // Normal textures
+    if (data.HasVertexTangent)
+    {
+        standard.NormalTexture = normalTex.Texture;
+        standard.NormalSampler = normalTex.Sampler;
+    }
+
+    // Alpha test
+    if (data.AlphaMode == Evergine.Framework.Runtimes.AlphaMode.Mask)
+    {
+        standard.AlphaCutout = data.AlphaCutoff;
+    }
+
+    // Vertex Color
+    if (data.HasVertexColor)
+    {
+        if (standard.ActiveDirectivesNames.Contains("VCOLOR"))
+        {
+            var directivesArray = standard.ActiveDirectivesNames;
+            Array.Resize(ref directivesArray, directivesArray.Length + 1);
+            directivesArray[directivesArray.Length - 1] = "VCOLOR";
+            standard.ActiveDirectivesNames = directivesArray;
+        }
+    }
+
+    return standard.Material;
+}
     }
 }
 
